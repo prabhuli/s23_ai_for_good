4b3aa620d118ef9d881e891ab538cf39698c6b0a
prabhuli <130502362+prabhuli@users.noreply.github.com>
2023-05-29 23:06:39 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -6,8 +6,9 @@ def compute_probs(neg,pos):
 def test_load():
   return 'loaded'
 
-def cond_prob(table, evidence, evidence_value, target, target_value):
-  t_subset = up_table_subset(table, target, 'equals', target_value)
-  e_list = up_get_column(t_subset, evidence)
-  p_b_a = sum([1 if v==evidence_value else 0 for v in e_list])/len(e_list)
-  return p_b_a + .01  #Laplace smoothing factor
+def cond_probs_product(table, e_val, t_col, t_val):
+  cond_prob_list =[]
+  table_colums = up_list_column_names(table)
+  evidence_columns = table_columns[:-1]
+  evidence_columns
+  evidence_complete = up_zip_lists(evidence_columns, e_val)
