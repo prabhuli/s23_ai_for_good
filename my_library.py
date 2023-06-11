98d90c21ef1767fffb7eefc524fa1e17fb2e5816
prabhuli <130502362+prabhuli@users.noreply.github.com>
2023-06-11 12:18:26 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -107,3 +107,25 @@ def run_random_forest(train, test, target, n):
   print(metrics_table)  #output we really want - to see the table
   return None
 
+def try_archs(full_table, target, architectures, thresholds):
+  train_table, test_table = up_train_test_split(full_table, target, .4)
+
+  #copy paste code here
+  #loop through your architecutes and get results
+
+  for arch in all_architectures:
+    all_results= up_neural_net(train_table, test_table, arch, target)
+  
+  #loop for t in thresholds:
+    all_mets = []
+    for t in thresholds:
+      all_predictions = [1 if pos>=t else 0 for neg,pos in all_results]
+      pred_act_list = up_zip_lists(all_predictions, up_get_column(test_table, target))
+      mets = metrics(pred_act_list)
+      mets['Threshold'] = t
+      all_mets = all_mets + [mets]
+
+  print(f'Architecture: {arch}')
+  print(up_metrics_table(all_mets))
+
+  return None  #main use is to print out threshold tables, not return anything useful.
